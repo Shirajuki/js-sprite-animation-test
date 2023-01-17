0075eb84a711dabf638f3081f4228726e19ab102
Shirajuki <shirajuki00@gmail.com>
2023-01-17 18:39:46 +0100

    chore: add initial vite template setup

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import { resolve } from 'path';
+
+const input = {};
+fs.readdirSync('src').forEach((file) => {
+  if (file.endsWith('.html')) {
+    if (file === 'index.html') return;
+    input[file.split('.')[0]] = resolve('src', file);
+  }
+});
+
+export default {
+  root: 'src',
+  base:
+    process.env.NODE_ENV === 'development'
+      ? './'
+      : '/js-sprite-animation-test/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve('src', 'index.html'),
+        ...input,
+      },
+    },
+  },
+};
