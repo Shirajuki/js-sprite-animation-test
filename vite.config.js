ceeea1df1b84a3bff1061e3997abff83cdec8e87
Shirajuki <shirajuki00@gmail.com>
2023-01-18 17:45:48 +0100

    feat: disably code minification

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,29 @@
-import fs from 'fs';
-import { resolve } from 'path';
+import fs from "fs";
+import { resolve } from "path";
 
 const input = {};
-fs.readdirSync('src').forEach((file) => {
-  if (file.endsWith('.html')) {
-    if (file === 'index.html') return;
-    input[file.split('.')[0]] = resolve('src', file);
-  }
+fs.readdirSync("src").forEach((file) => {
+	if (file.endsWith(".html")) {
+		if (file === "index.html") return;
+		input[file.split(".")[0]] = resolve("src", file);
+	}
 });
 
 export default {
-  root: 'src',
-  base:
-    process.env.NODE_ENV === 'development'
-      ? './'
-      : '/js-sprite-animation-test/',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve('src', 'index.html'),
-        ...input,
-      },
-    },
-  },
+	root: "src",
+	base:
+		process.env.NODE_ENV === "development"
+			? "./"
+			: "/js-sprite-animation-test/",
+	build: {
+		outDir: "../dist",
+		minify: false,
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve("src", "index.html"),
+				...input,
+			},
+		},
+	},
 };
